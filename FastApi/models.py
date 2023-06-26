1f8b5f7e81d6290289b19392497f9c710c5a41e6
osvafernandez <osvanaranjo96@gmail.com>
2023-06-26 09:30:57 -0400

    models init

--- FastApi/models.py
+++ FastApi/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Boolean, Float
 
 
 class Transaction(Base):
-    _tablename_ = 'transactions'
+    __tablename__ = 'transactions'
 
     id = Column(Integer, primary_key=True, index=True)
     amount = Column(Float)
